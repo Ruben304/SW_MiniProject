4a6d55516b5ea99bb9be6627a56e11d96b811d44
Johnsony0 <johnsony@bu.edu>
2023-09-13 17:36:06 -0400

    can now send messages and will show on firebase

--- miniproj/src/App.js
+++ miniproj/src/App.js
@@ -5,6 +5,8 @@ import GoogleButton from 'react-google-button';
 import {signInWithPopup, GoogleAuthProvider, signOut} from 'firebase/auth'
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 function App(){
     const [user] = useAuthState(auth);
@@ -59,6 +61,20 @@ function Room() {
     const messagesRef = firestore.collection('messages');
     const query = messagesRef.orderBy('createdAt').limit(25);
     const [messages] = useCollectionData(query,{idField: 'id'});
+    const [formValue,setFormValue] = useState('');
+
+    const sendMessage = async(e) =>{
+        e.preventDefault();
+        const {uid, photoURL} = auth.currentUser;
+
+        await messagesRef.add({
+            text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL
+        })
+        setFormValue(' ');
+    }
 
     return(
         <>
@@ -68,6 +84,10 @@ function Room() {
             <div>
                 {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
             </div>
+            <form onSubmit={sendMessage}>
+                <input value = {formValue} onChange={(e) => setFormValue(e.target.value)}/>
+                <button type="submit">send message</button>
+            </form>
         </>
     ) 
 }
